081f4a22c84557277fe6c00e01e9ab82da94fdfb
Nicolas Sias <nicolas.sias@gmail.com>
2025-07-15 23:31:37 +0200

    Add User + Auth

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Créer l'utilisateur demo
+        $user = new User();
+        $user->setEmail('demo@symfony.fr');
+        $user->setRoles(['ROLE_USER']);
+        
+        // Hasher le mot de passe
+        $hashedPassword = $this->passwordHasher->hashPassword($user, 'azerty');
+        $user->setPassword($hashedPassword);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
