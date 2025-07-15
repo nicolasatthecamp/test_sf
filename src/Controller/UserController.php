ccc72e4ebda87cdeab25d3101da6cc27cae6e694
Nicolas Sias <nicolas.sias@gmail.com>
2025-07-15 23:14:49 +0200

    Add user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/users')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_users_list')]
+    public function list(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('user/list.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
